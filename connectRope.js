afa429819c842c89c372ed317c12dca83a21b46b
KingJeremyNg <jeremy97ng@gmail.com>
2021-03-12 13:59:25 -0500

    Completed A3 Q2

--- connectRope.js
+++ connectRope.js
@@ -1,10 +1,35 @@
 /*
+CPS 616 Assignment 3 Question 1
+Jeremy Ng
+500882192
+*/
+
+/*
 Suppose there exist n ropes. They have different length. The goal is to connect them so that they
 become one piece. The cost of connecting rope 1 and rope 2 equals to the sum of the length of
 rope 1 and rope 2, plus 1. Bring a greedy algorithm which connect all n ropes with the
 minimum cost.
 */
 
-function connectRope(arr) {
+/*
+To solve the above problem, I connect the 2 smallest ropes and accumulate the cost.
+Then I remove the 2 ropes from the array.
+I then put the newly created rope back into the array.
+I follow this until I am left with 1 rope in my array and return the cost variable.
+*/
 
+function connectRope(arr) {
+    let cost = 0;
+    while (arr.length !== 1) {
+        let min1 = Math.min(...arr);        // ...arr is a spread operator
+        arr.splice(arr.indexOf(min1), 1);   // splice(index, num) returns a subarray and removes the elements from the original array.
+        let min2 = Math.min(...arr);
+        arr.splice(arr.indexOf(min2), 1);
+        arr.push(min1 + min2);              // Add the new rope back into arr
+        cost += min1 + min2 + 1;
+    }
+    return cost;
 }
+
+arr = [1, 2, 3, 4];
+console.log(connectRope(arr));
--- search.js
+++ search.js
@@ -1,8 +1,28 @@
 /*
+CPS 616 Assignment 3 Question 1
+Jeremy Ng
+500882192
+*/
+
+/*
 Consider a 2D array of integers, so that elements in each row are sorted. The elements of each
 column are sorted as well. Both rows and columns are sorted in increasing order. Propose a
 divide and concur approach to find whether a specific value is in the array. Analysis the time
 complexity of your approach.
+*/
+
+/*
+Below I made a program that solves the above problem by using a divide and conquer approach.
+I Found that the value that I am searching for can only be in 3 of 4 quartiles based on a midpoint.
+Time complexity of my program is:
+    = 7 + m/2 * (n Log n + 2) + m/2 * (n/2 Log n/2 + 2)
+Time complexity of my searchQuartile function is (n Log n).
+I am always searching 2 quartiles at the same time and 1 quartile separately.
+Assuming that m = n then we have:
+    = 7 + n/2 * (n Log n + 2) + n/2 * (n/2 Log n/2 + 2)
+    = 7 + (n^2)/2 * Log n + n + (n^2)/4 log n/2 + n
+    = 7 + 2n + (n^2)/2 * Log n + (n^2)/4 log n/2
+Therefore my final time complexity is O(n Log n)
 */
 
 function search(arr, num) {
@@ -10,29 +30,29 @@ function search(arr, num) {
     const col = arr[0].length;
     const mid_i = Math.floor(row / 2);
     const mid_j = Math.floor(col / 2);
+    if (arr[mid_i][mid_j] === num) return { i: mid_i, j: mid_j };   // Return i and j if num is equal to the midpoint
     let ans;
-    if (arr[mid_i][mid_j] === num) return { i: mid_i, j: mid_j };       // Return i and j if num is the midpoint
-    else if (num < arr[mid_i][mid_j]) {                                 // Search quartile 1, 2, 3
-        for (let i = 0; i < mid_i; i++) {                               // Search quartile 1, 2
+    if (num < arr[mid_i][mid_j]) {                                  // Search quartile 1, 2, 3
+        for (let i = 0; i < mid_i; i++) {                           // Search quartile 1, 2
             ans = searchQuartile(i, 0, col - 1);
             if (ans) return ans;
         }
-        for (let i = mid_i; i < row; i++) {                             // Search quartile 3
+        for (let i = mid_i; i < row; i++) {                         // Search quartile 3
             ans = searchQuartile(i, 0, mid_j - 1);
             if (ans) return ans;
         }
     }
-    else if (num > arr[mid_i][mid_j]) {                                 // Search quartile 2, 3, 4
-        for (let i = 0; i < mid_i; i++) {                               // Search quartile 3, 4
-            ans = searchQuartile(i, mid_i, col - 1);
+    if (num > arr[mid_i][mid_j]) {                                  // Search quartile 2, 3, 4
+        for (let i = mid_i; i < mid_i; i++) {                       // Search quartile 3, 4
+            ans = searchQuartile(i, 0, col - 1);
             if (ans) return ans;
         }
-        for (let i = mid_i; i < row; i++) {                             // Search quartile 2
-            ans = searchQuartile(i, mid_i, col - 1);
+        for (let i = 0; i < row; i++) {                             // Search quartile 2
+            ans = searchQuartile(i, mid_j, col - 1);
             if (ans) return ans;
         }
     }
-    function searchQuartile(i, start, end) {
+    function searchQuartile(i, start, end) {                        // Function to search for num by cutting array into halves each step
         while (start < end) {
             let mid = Math.floor((start + end) / 2);
             if (arr[i][mid] < num) start = mid + 1;
@@ -48,6 +68,7 @@ arr = [[10, 20, 30, 40],
 [14, 24, 34, 44],
 [16, 26, 36, 46]];
 
+// Searching each element in arr
 arr.forEach(row => {
     row.forEach(element => {
         console.log(search(arr, element), element);
